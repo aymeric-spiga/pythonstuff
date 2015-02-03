4183574d97a5463f7d84984a9e321717fedef6c2
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2015-02-03 23:03:07 +0100

    added coeff to wind prof

--- profwind.py
+++ profwind.py
@@ -1,13 +1,16 @@
 #! /usr/bin/env python
 import numpy as np
 import ppplot as ppp
-phalf = 100.
-press = np.logspace(6,-6)
+phalf = 2.e5
+transi = 3.
+transi = 2.
+
+press = np.logspace(6,0)
 pl=ppp.plot1d()
 pl.f = press
-pl.x = 0.5*(1+np.tanh(np.log(press/phalf)))
+pl.x = 0.5*(1+np.tanh(transi*np.log(press/phalf)))
 pl.fmt = "%2.e"
 pl.logy = True
-pl.logx = True
+#pl.logx = True
 pl.invert = True
 pl.makeshow()
