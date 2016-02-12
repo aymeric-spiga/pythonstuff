1b015ee416935bef3da4ea53e17a10d6370e3122
Aymeric Spiga <aymeric.spiga@lmd.jussieu.fr>
2016-02-12 11:15:02 +0100

    cosmetic

--- curvefit/vent_solaire.py
+++ curvefit/vent_solaire.py
@@ -11,7 +11,7 @@
 
 # Commençons par charger les données au format `ASCII` en utilisant la fonction `loadtxt`. Cette fonction est dans la librairie `numpy` que l'on doit donc importer. Par ailleurs, comme nous allons utiliser `curve_fit` et faire des figures, importons également les librairies `scipy.optimize` et `matplotlib`. 
 
-# In[1]:
+# In[31]:
 
 import numpy as np
 import scipy.optimize as sciopt
@@ -26,14 +26,14 @@ get_ipython().magic(u'matplotlib inline')
 # * densité $\rho$ stockée en colonne 2 en cm$^{-3}$
 # * pression $P$ en colonne 3 en K cm$^{-3}$
 
-# In[2]:
+# In[32]:
 
 dist,dens,press = np.loadtxt("ulysse_first.txt",unpack=True)
 
 
 # Nous pouvons immédiatement vérifier la quantité de points collectés par ULYSSE que nous venons de charger, ainsi que, par exemple, les valeurs minimum et maximum de chacune des variables
 
-# In[3]:
+# In[33]:
 
 print dist.shape,dens.shape,press.shape
 print dist.min(),dens.min(),press.min()
@@ -42,7 +42,7 @@ print dist.max(),dens.max(),press.max()
 
 # Nous allons convertir la pression $P$ en megaPascal en multipliant par la constante de Boltzmann les valeurs en cm$^{-3}$
 
-# In[4]:
+# In[34]:
 
 import scipy.constants as scicst
 press = scicst.k * press
@@ -50,7 +50,7 @@ press = scicst.k * press
 
 # Nous pouvons par ailleurs faire une figure, par exemple $\rho$ en fonction de $r$. Nous allons représenter les mesures par des points rouges pour bien souligner qu'il s'agit d'une succession de mesures ponctuelles.
 
-# In[5]:
+# In[35]:
 
 mpl.plot(dist,dens,'r.',label="mesures ULYSSE") # commande principale
 mpl.xlabel(u'distance héliocentrique(UA)') # titre axe abscisses
@@ -66,7 +66,7 @@ mpl.legend() # affichage de la légende
 # 
 # Commençons donc par définir ladite fonction paramétrique $f_{a,b}$ que nous appellerons en Python `regf` (pour fonction de régression).
 
-# In[6]:
+# In[36]:
 
 def regf(x,a,b):
     return a * (x**b)
@@ -74,7 +74,7 @@ def regf(x,a,b):
 
 # Pour illustrer le principe de fonction paramétrique $f_{a,b}$, dessinons `regf` pour des valeurs différentes de `a` et `b`
 
-# In[7]:
+# In[37]:
 
 x = np.linspace(-6,6,100)
 a = 1 ; b = 2 ; mpl.plot(x,regf(x,a,b),label='$x^2$ (a=%i et b=%i)' % (a,b))
@@ -97,14 +97,14 @@ mpl.xlabel('$x$') ; mpl.ylabel('$y$')
 # 
 # L'appel à la fonction `curve_fit` s'écrit donc dans notre cas
 
-# In[8]:
+# In[38]:
 
 param,cov = sciopt.curve_fit(regf,dist,dens)
 
 
 # Voyons les paramètres obtenus par `curve_fit`
 
-# In[9]:
+# In[39]:
 
 a_opt = param[0]
 b_opt = param[1]
@@ -115,7 +115,7 @@ print u'résultat a=%.3f et b=%.3f' % (a_opt,b_opt)
 
 # Au vu du résultat obtenu pour $b$, il est clair que nous n'avons pas $\rho$ proportionnel à $r$ d'après les données ULYSSE ! Ajoutons la fonction $f_{a,b}$ pour les paramètres optimaux $a_{opt}$ et $b_{opt}$ à la figure précédemment produite avec les mesures ULYSSE
 
-# In[10]:
+# In[40]:
 
 mpl.plot(dist,dens,'r.',label="mesures ULYSSE") 
 mpl.xlabel(u'distance héliocentrique(UA)')
@@ -137,7 +137,7 @@ mpl.legend()
 # 
 # Nous allons donc restreindre l'analyse aux données obtenues pour $r>1.5$ UA. Pour cela, il suffit d'employer la fonction `where` de `numpy` qui permet de sélectionner une partie d'un tableau vérifiant une condition particulière, pour l'utiliser ensuite pour définir un nouveau tableau.
 
-# In[11]:
+# In[41]:
 
 w = np.where(dist > 1.5)
 dist2 = dist[w]
@@ -146,7 +146,7 @@ dens2 = dens[w]
 
 # Nous pouvons appliquer de nouveau `curve_fit' comme précédemment. 
 
-# In[12]:
+# In[42]:
 
 param,cov = sciopt.curve_fit(regf,dist2,dens2)
 a_opt = param[0]
@@ -156,7 +156,7 @@ print u'résultat a=%.3f et b=%.3f' % (a_opt,b_opt)
 
 # Cette fois, en ayant restreint les mesures de $\rho$ au cas où $r>1.5$ UA, nous trouvons $b \simeq -2$ comme prévu théoriquement. Nous pouvons reprendre le graphique précédemment effectué pour constater cette fois la bien meilleure performance de `curvefit`.
 
-# In[13]:
+# In[43]:
 
 mpl.plot(dist2,dens2,'r.',label="mesures ULYSSE") 
 mpl.xlabel(u'distance héliocentrique(UA)')
@@ -169,16 +169,16 @@ mpl.legend()
 
 # Considérons désormais la possibilité que l'expansion du vent solaire soit un processus isotherme. La pression $P$ et la densité $\rho$ doivent alors vérifier $ P / \rho = \alpha $ avec $\alpha$ une constante, ce qui indique que $P$ doit suivre une loi en $r^{-2}$ comme $\rho$. Vérifions cela en appliquant la même méthode que précédemment et en se restreignant au domaine $r > 1.5$ UA.
 
-# In[14]:
+# In[44]:
 
 press2 = press[w]
 param,covp = sciopt.curve_fit(regf,dist2,press2)
 a_opt = param[0]
 b_opt = param[1]
-print u'résultat a=%.3f et b=%.3f' % (a_opt,b_opt)
+print u'résultat a=%5.2e et b=%.3f' % (a_opt,b_opt)
 
 
-# In[15]:
+# In[45]:
 
 mpl.plot(dist2,press2,'r.',label="mesures ULYSSE") 
 mpl.xlabel(u'distance héliocentrique(UA)')
